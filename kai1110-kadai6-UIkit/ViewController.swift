03f9411811c405817548efb7f6716f3d9c8b4269
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-05 16:52:06 -0800

    Initial Commit

---
+++ kai1110-kadai6-UIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kai1110-kadai6-UIkit
+//
+//  Created by 渡邊魁優 on 2023/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
