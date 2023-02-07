b83a5aa9a3c2b9782afe7f43a88730a16ee6a864
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-07 23:56:29 +0900

    commit

--- kai1110-kadai6-UIkit/ViewController.swift
+++ kai1110-kadai6-UIkit/ViewController.swift
@@ -1,19 +1,50 @@
-//
-//  ViewController.swift
-//  kai1110-kadai6-UIkit
-//
-//  Created by 渡邊魁優 on 2023/02/05.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Score: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    private var labelValue = Int.random(in: 0...100)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateLabel()
     }
-
-
+    
+    @IBAction func Judg(_ sender: Any) {
+        let sliderValue = Int(slider.value)
+        if sliderValue == labelValue {
+            resultAlert(message: "あたり")
+        }
+        else {
+            resultAlert(message: "はずれ")
+        }
+    }
+    
+    private func resultAlert(message: String) {
+        let alertcontroller = UIAlertController(
+            title: "結果",
+            message: message + "\nあなたの値は\(Int(slider.value))",
+            preferredStyle: .alert
+        )
+        let OKButton = UIAlertAction(
+            title: "再挑戦",
+            style: .default,
+            handler: { _ in
+                self.retry()
+            }
+        )
+        alertcontroller.addAction(OKButton)
+        present(alertcontroller, animated: true)
+    }
+    
+    private func updateLabel() {
+        Score.text = String(labelValue)
+    }
+    
+    private func retry() {
+        labelValue = Int.random(in: 0...100)
+        updateLabel()
+    }
 }
-
